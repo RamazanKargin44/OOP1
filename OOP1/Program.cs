d790b44b70e3b06f404db1778277753577647379
RamazanKargin44 <ramazankargin@outlook.com>
2021-05-11 01:30:43 +0300

    Add project files.

---
+++ OOP1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 2;
+            product1.ProductName = "masa";
+            product1.UnitPrice = 150;
+            product1.UnitsInStock = 3;
+
+            Product product2 = new Product { 
+                Id = 2,
+                CategoryId = 2,
+                ProductName = "sandalye",
+                UnitsInStock = 3,
+                UnitPrice = 40 
+            };
+            ProductManager productManager = new ProductManager();
+
+            productManager.Add(product1);
+
+
+        }
+    }
+}
